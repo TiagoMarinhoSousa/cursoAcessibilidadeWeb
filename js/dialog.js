7725477153e1558b7b8ed5ef610319450d86a2d7
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2021-03-25 15:05:18 -0300

    A

--- js/dialog.js
+++ js/dialog.js
@@ -1,5 +1,6 @@
 // Variáveis
-var conteudoForaDialog = document.querySelector('#conteudoForaDialog')
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+// var video = document.querySelector('video');
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
@@ -13,6 +14,8 @@ btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
   document.querySelector('.dialogNewsletter-label').focus();
   conteudoForaDialog.inert = true;
+
+  // video.removeAttribute('controls');
 });
 
 function fechandoDialog() {
@@ -20,7 +23,29 @@ function fechandoDialog() {
   dialog.classList.remove('dialogNewsletter--aberto'); 
   conteudoForaDialog.inert = false;   
   btnAbreDialog.focus(); 
+
+  // video.setAttribute('controls', true);
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+dialogOverlay.addEventListener('click', fechandoDialog);
+
+
+document.addEventListener('keydown', function(event) { 
+  if (event.key === 'Escape'){
+    fechandoDialog();
+  }
+})
+
+// $(document).keypress(function(event) { 
+//   if (event.keyCode == 27) { 
+//     fechandoDialog();
+//   }
+// });
+
+// document.addEventListener('keydown', (event) => {
+//   if (event.key === 'Escape') {
+//     fechandoDialog();
+//   }
+// })
